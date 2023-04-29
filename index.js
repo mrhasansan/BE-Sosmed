f143d042e6885cb9835a229e56787d3c3ef9d73b
M Haidar Hanif <git@mhaidarhanif.com>
2023-04-29 20:15:07 +0700

    fix: ignore node_modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const cors = require("cors");
